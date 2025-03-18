01ec44d0417201f13002b904ee654ebf5a93024b
zaihuaji <zaihuaji@gmail.com>
2025-03-18 11:30:06 -0500

    empty code

--- tests/test_setuid.py
+++ tests/test_setuid.py
@@ -1,11 +1,6 @@
-# test_hello_world.py
+# test_setuid.py
 
 import pytest
-from src.rda_python_template.hello_world import get_string
 
-def test_get_string():
-   assert get_string('Bob') == 'Bob: Hello World!'
-
-def test_raises_exception_on_non_string_arguments():
-   with pytest.raises(TypeError):
-      get_string(9)
+def test_setuid():
+   pass
